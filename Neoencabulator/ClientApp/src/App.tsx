e18bf30442181831507fdad58eadf739303f9b9d
Joe <jvillarreal@wsu.edu>
2022-01-12 19:34:55 -0800

    Added material UI to the project, and made a button. How do I make a REST call?

--- Neoencabulator/ClientApp/src/App.tsx
+++ Neoencabulator/ClientApp/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
+
 import './App.css';
 
 function App() {
+
+  const [messge, setMessage] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">
+        <div>
+            <Button color="primary" variant="contained">
+                REST
+             </Button>
+        </div>
+        <div>
+                  RESTful Replay:
+        </div>
     </div>
   );
 }
