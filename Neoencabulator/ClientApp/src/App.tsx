cadb5d822be6f57cd45522ad56c89dbb2cce79db
Joe <jvillarreal@wsu.edu>
2022-02-09 22:07:00 -0800

    added Axios, trying to have Axios 'get' a string from the backend.

--- Neoencabulator/ClientApp/src/App.tsx
+++ Neoencabulator/ClientApp/src/App.tsx
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
 import { Button } from '@material-ui/core';
-
+import axios from 'axios';
 import './App.css';
 
 function App() {
+    const axios = require('axios').default;
 
-  const [messge, setMessage] = useState("");
+    const [message, setMessage] = React.useState("");
 
   return (
       <div className="App">
-        <div>
-            <Button color="primary" variant="contained">
+          <div>
+              <Button color="primary" variant="contained"
+                  onClick={() => {
+                      axios.get('/user')
+                          .then(function (response: string) {
+                              console.log(response);
+                              setMessage(response);
+                          })
+                  }
+              }>
                 REST
              </Button>
         </div>
         <div>
-                  RESTful Replay:
+              RESTful Replay: {message}
         </div>
     </div>
   );
---
+++ Neoencabulator/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neoencabulator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public UsersController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var returnValue = "Joe V was here";
+            return returnValue;
+        }
+    }
+}
