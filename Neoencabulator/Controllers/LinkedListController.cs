937eec9c04d78c56df7c9c49281f759379fbfd5e
zelklen <zelklen@gmail.com>
2022-08-17 18:37:44 -0700

    Added controller for the Linked List workspace

---
+++ Neoencabulator/Controllers/LinkedListController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Neoencabulator.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neoencabulator.Controllers
+{
+    public class LinkedListPoco
+    {
+        // The structure in this type, must match the POST request object's body EXACTLY. Including names.
+        public string item { get; set; }
+    }
+
+    [ApiController]
+    [Route("[controller]")]
+    public class LinkedListController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public LinkedListController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("add")]
+        public void AddItem(LinkedListPoco post)
+        {
+            LinkedListLogic.addNode(post.item);
+        }
+
+        [HttpPost("remove")]
+        public void RemoveName(LinkedListPoco post)
+        {
+            LinkedListLogic.removeNode(post.item);
+        }
+
+        [HttpGet]
+        public List<string> Get()
+        {
+            //return new List<string> { "Works." };
+            return LinkedListLogic.getList();
+        }
+    }
+}
---
+++ Neoencabulator/Logic/LinkedListLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Neoencabulator.Logic
+{
+    public static class LinkedListLogic
+    {
+        private static List<string> names = new List<string>();
+
+        public static List<string> getList()
+        {
+            return names;
+        }
+
+        public static bool addNode(string name)
+        {
+            try
+            { // Is add even capable of throwing?
+                names.Add(name);
+            }
+            catch(System.Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool removeNode(string name)
+        {
+            return names.Remove(name);
+        }
+    }
+}
