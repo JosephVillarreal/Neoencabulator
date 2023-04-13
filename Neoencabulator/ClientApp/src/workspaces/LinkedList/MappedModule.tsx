bd969fb5d1e0d84c71440a1e6d4d9990f37b1ff4
zelklen <zelklen@gmail.com>
2023-04-12 17:24:10 -0700

    Completed linked list

--- Neoencabulator/ClientApp/src/workspaces/LinkedList/MappedModule.tsx
+++ Neoencabulator/ClientApp/src/workspaces/LinkedList/MappedModule.tsx
@@ -10,40 +10,51 @@ type MappedModuleProps = {
   deleteHandler: Function,
 }
 
+// Rename this function to be something like "LinkedListNodeView"
 function MappedModule(props: MappedModuleProps) {
   return (
-    <>
-      <TextField
-        value={props.insertContent}
-        onChange={
-          (e: React.ChangeEvent<HTMLInputElement>) =>
-          {
-            props?.setInsertContentHandler(e.target.value, props.id);
+    <div>
+      <div
+        id="stop"
+        style={{ height: '50px' }}
+      >
+        <Button color="primary" variant="contained"
+          onClick={() => {
+            props.insertHandler(props.id, props.insertContent);
+          }}
+        >
+          +
+        </Button>
+        <TextField
+          value={props.insertContent}
+          onChange={
+            (e: React.ChangeEvent<HTMLInputElement>) =>
+            {
+              props?.setInsertContentHandler(e.target.value, props.id);
+            }
           }
-        }
+          size="small"
+        >
+        </TextField>
+      </div>
+      <div
+        id="appendRowDiv"
+        style={{ display: 'flex', height: '50px' }}
       >
-      </TextField>
-      <Button color="primary" variant="contained"
-        onClick={() => {
-          props.insertHandler(props.id, props.insertContent);
-        }}
-      >
-        +
-      </Button>
-      <br />
-      <Typography>
-        {
-          props.content
-        }
-      </Typography>
-      <Button color="primary" variant="contained"
-        onClick={() => {
-          props.deleteHandler(props.id);
-        }}
-      >
-        -
-      </Button>
-    </>
+        <Button color="primary" variant="contained"
+          onClick={() => {
+            props.deleteHandler(props.id);
+          }}
+          style={{ height: '36.5px' }}
+        >
+          -
+        </Button>
+        <TextField
+          inputProps={{ readOnly: true, value: props.content }}
+          size="small"
+        />
+      </div>
+    </div>
   )
 }
 
