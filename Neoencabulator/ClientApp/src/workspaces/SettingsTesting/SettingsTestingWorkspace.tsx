9d5a459d12ca603986c74f4f36b8a8441529f57d
zelklen <zelklen@gmail.com>
2023-08-09 17:52:53 -0700

    Gettings started with the settings testing workspace.

---
+++ Neoencabulator/ClientApp/src/workspaces/SettingsTesting/SettingsTestingWorkspace.tsx
@@ -0,0 +1,59 @@
+﻿import { makeStyles } from '@mui/styles';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Button, TextField } from '@mui/material';
+
+const useStyles = makeStyles({
+  exceptionTextField: {
+    whiteSpace: 'pre-wrap',
+    height: '500px',
+    width: '500px'
+  }
+});
+
+function SettingsTestingWorkspace() {
+  const classes = useStyles();
+
+  function runSingle(input: string) {
+    axios.get('Exceptions', { params: { input: input } })
+      .then((response: any) => {
+        console.log(response);
+        setResult(response.data);
+      })
+      .catch(function (error: any) {
+        console.log(error);
+      })
+      .then(function () {
+        console.log("End of Axios Post");
+      });
+  }
+  function runDouble(inputOne: string, inputTwo: string) {
+    axios.get('Exceptions', { params: { inputA: inputOne, inputB: inputTwo } })
+      .then((response: any) => {
+        console.log(response);
+        setResult(response.data);
+      })
+      .catch(function (error: any) {
+        console.log(error);
+      })
+      .then(function () {
+        console.log("End of Axios Post");
+      });
+  }
+
+  const [exceptionQuestion, setExceptionQuestion] = useState<number>(0);
+  const [result, setResult] = useState<string>("");
+
+  return (
+    <div>
+      <div>
+        <TextField
+          inputProps={{ readOnly: true, value: result }}
+          size="small"
+          label="Result"
+        />
+      </div>
+    </div>
+  );
+}
+export default SettingsTestingWorkspace;
---
+++ Neoencabulator/Controllers/SettingsTestingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Neoencabulator.Logic;
+using Neoencabulator.Logic.ExceptionsLogic;
+using System;
+
+namespace Neoencabulator.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class SettingsTestingController : Controller
+  {
+    [HttpGet]
+    public string Get(string input)
+    {
+      return input;
+    }
+
+    [HttpGet]
+    public string Get(string inputOne, string inputTwo)
+    {
+      return (inputOne+inputTwo);
+    }
+  }
+}
